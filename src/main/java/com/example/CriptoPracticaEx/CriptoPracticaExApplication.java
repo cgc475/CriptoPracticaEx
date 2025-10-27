159919a70b124e0fc8d54535abab39da211ffdcd
CARLOS <carlosgiraldo4755@gmail.com>
2025-10-27 15:38:22 -0500

    Se movio el aplication

---
+++ src/main/java/com/example/CriptoPracticaEx/CriptoPracticaExApplication.java
@@ -0,0 +1,13 @@
+package com.example.CriptoPracticaEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptoPracticaExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptoPracticaExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CriptoPracticaEx/CriptoPracticaExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CriptoPracticaEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptoPracticaExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
